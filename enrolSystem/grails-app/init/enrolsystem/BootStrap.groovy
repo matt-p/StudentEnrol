f9df7f4455c39513d9f7990bc3f7009c540c95d9
matt-p <b5014880@my.shu.ac.uk>
2017-01-16 13:39:23 +0000

    16-01 Adding new domainclass etc

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,8 +1,37 @@
-package enrolsystem
+package com.enrol
 
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = { servletContext -> 
+	def computing = new Course(
+		title:'BSc Hon Computing',
+		code:'COMP234',
+		leader:'Dr Mary Poppins',
+		department:'Computing',
+		startDate:new Date('11/11/2016'),
+		endDate:new Date('11/11/2020'),
+		numberOfStudents:45,
+		descripton:'''Add some text here''',
+		tuitionFees:9000,
+		studyMode:'Full-time'
+	).save()
+	def system = new Module(
+		title:'Systems Arch',
+		code:'SA567',
+		credits:'1',
+		lecturer:'Dr Sue Jenkins',
+		course:'BSc Hons Computing',
+		description:'Module includes working with virtual machines'
+	).save()
+	def lectut = new Lecturer(
+		fullName:'Dr Joe Bloggs',
+		post:'post',
+		subject:'Computing',
+		lecturerEmail:'bloggs@gmail.com',
+		office:'Charles St',
+		bio:'I am a lecturer'
+	).save()
+
     }
     def destroy = {
     }
